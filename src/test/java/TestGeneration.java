fea50b03bbcbf643a4784cb3c00d17becf263940
ARUBA\claudio.tortorelli <claudio.tortorelli@staff.aruba.it>
2023-10-28 20:19:50 +0200

    update test generation

--- src/test/java/TestGeneration.java
+++ src/test/java/TestGeneration.java
@@ -3,10 +3,17 @@
  *
  *
  */
+import claudiosoft.pocbase.BasicConsoleLogger;
 import claudiosoft.pocbase.POCException;
+import static claudiosoft.readableotp.OTPConstants.PART_2;
+import static claudiosoft.readableotp.OTPConstants.SCORE_3;
 import claudiosoft.readableotp.OTPGenerator;
+import claudiosoft.readableotp.OTPRule;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 import org.junit.FixMethodOrder;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
@@ -14,7 +21,53 @@ import org.junit.runners.MethodSorters;
 public class TestGeneration extends BaseJUnitTest {
 
     @Test
-    public void t01Generation1() throws InterruptedException, IOException, POCException {
+    public void t01GenerationRules() throws InterruptedException, IOException, POCException {
+        OTPGenerator gen = new OTPGenerator();
+        List rules = new LinkedList<>();
+
+        rules.add(new OTPRule("xxxxxx", "[0,9]", SCORE_3, PART_2));
+        gen.overrideRules(rules);
+        BasicConsoleLogger.get().info(gen.generate().get());
+
+//        rules.clear();
+//        rules.add(new OTPRule("xxxyyy", "[0,9]", "!", SCORE_2, PART_2));
+//        gen.overrideRules(rules);
+//        BasicConsoleLogger.get().info(gen.generate().get());
+//
+//        rules.clear();
+//        rules.add(new OTPRule("xxxyyy", "[0,9]", "!", SCORE_2, PART_2));
+//        gen.overrideRules(rules);
+//        BasicConsoleLogger.get().info(gen.generate().get());
+//
+//        rules.clear();
+//        rules.add(new OTPRule("xyyxyy", "[0,9]", "!", SCORE_2, PART_2));
+//        gen.overrideRules(rules);
+//        BasicConsoleLogger.get().info(gen.generate().get());
+//
+//        rules.clear();
+//        rules.add(new OTPRule("xyxxyx", "[0,9]", "!", SCORE_2, PART_2));
+//        gen.overrideRules(rules);
+//        BasicConsoleLogger.get().info(gen.generate().get());
+
+//        rules.clear();
+//        rules.add(new OTPRule("xxyyxx", "[0,9]", "!", SCORE_2, PART_2));
+//        gen.overrideRules(rules);
+//        BasicConsoleLogger.get().info(gen.generate().get());
+//
+//        rules.clear();
+//        rules.add(new OTPRule("xyyyyx", "[0,9]", "!", SCORE_2, PART_2));
+//        gen.overrideRules(rules);
+//        BasicConsoleLogger.get().info(gen.generate().get());
+
+//        rules.clear();
+//        rules.add(new OTPRule("xyzxyz", "[0,9]", "!", SCORE_2, PART_2));
+//        gen.overrideRules(rules);
+//        BasicConsoleLogger.get().info(gen.generate().get());
+    }
+
+    @Test
+    @Ignore
+    public void t02GenerationRandom() throws InterruptedException, IOException, POCException {
         OTPGenerator gen = new OTPGenerator();
         for (int i = 0; i < 10; i++) {
             System.out.println(gen.generate().get());
